1af5d38c978b940bd43ed337f34893c0c67da8ed
Precious Michael <precious.michael2002@gmail.com>
2026-04-23 11:34:46 +0100

    update

--- src/main/java/org/dreamcommerce/dreamcommerce/service/ProductServiceImpl.java
+++ src/main/java/org/dreamcommerce/dreamcommerce/service/ProductServiceImpl.java
@@ -42,6 +42,7 @@ public class ProductServiceImpl implements ProductService {
 
         //save
         Product savedProduct = productRepository.save(product);
+
         return modelMapper.map(savedProduct, UpdateProductResponse.class);
     }
 }
